666a740c03b7f78343f1aac0495363821e0c1753
BUI_PHONG_HOP <hop.buikhmtk2025@hcmut.edu.vn>
2026-06-23 01:48:56 +0700

    update some EXE of Recursion

--- LAB-RECURSION.cpp
+++ LAB-RECURSION.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<algorithm>
+#include<string>
+#include<string.h>
 using namespace std;
 
-/* Function to print 0 -> n */
+/* Question 1 */
 void printArray(int n){
     // base case
     if(n == 0){
@@ -16,10 +19,7 @@ void printArray(int n){
 // Time complexity: O(n)
 
 
-/*
-Print follow pattern: n = 16
-Output: 16 11 6 1 -4 1 11 16
-*/
+/* Question 2 */
 void printPatten(int n){
     // base case
     if(n <= 0){
@@ -32,14 +32,116 @@ void printPatten(int n){
     printPatten(n - 5);
     cout << n << " "; 
 }
+// Time complexity: O(n)
 
 
-/* find the largest element by recursion */
+/* Question 3 */
 int findMax(int *arr , int length){
-    
-}
- 
+    // base case
+    if(length == 1) return arr[0];
 
-int main(){
-    printPatten(16);
+    // recursive step
+    return max(arr[length - 1] , findMax(arr , length - 1));
+}
+// Time complexity: O(n)
+
+
+/* Question 5 */
+// This algorithm call Euclidiean algorithm: GCD(a , b) = GCD(b , a % b)
+int findGCD(int a , int b){
+    // base case
+    if(b == 0) return a;
+    
+    // recursive step
+    return findGCD(b , a % b);
+}
+// Time complexity: O(log(min(a , b)))
+// BONUS: find the LCM (Lowest Common Multiple)
+// LCM(a , b) = (a * b) / GCD(a , b)
+int findLCM(int a , int b){
+    // Divide first to collect the smaller number before product
+    return (a / findGCD(a , b)) * b;
+}
+
+
+/* Question 6 */
+string expand_support(string s , int &index){
+    string result = "";
+
+    while(index < (int)s.size() && s[index] != ')'){
+        // if the character is a number -> recursive step
+        if(isdigit(s[index])){
+            // number of repeating sequences
+            int numberStep = s[index] - '0';
+            index += 2; // point to the character after '('
+
+            // save the string inside()
+            string tmp = expand_support(s , index);
+
+            while(numberStep-- > 0){
+                result += tmp;
+            }
+
+            index++; // point to the character ')'
+        }
+
+        // if the character is lowercase word
+        else if(s[index] >= 'a' && s[index] <= 'z'){
+            result += s[index];
+            index++;
+        }
+    }    
+    return result;
+}
+string expand(string s){
+    int index = 0;
+    return expand_support(s , index);
+}
+// Time complexity: O(|s|), s is the length of the output string
+
+
+/* Question 7 */
+// Hailstone string
+void printHailstone(int number){
+    cout << number << " ";
+
+    if(number == 1){
+        return;
+    }
+
+    if(number % 2 == 0){
+        number /= 2;
+        printHailstone(number);
+    }
+
+    else{
+        number = number * 3 + 1;
+        printHailstone(number);
+    }
+}
+
+
+/* Question 10 */
+int solve(string s, int index, int open_needed, int added) {
+    // base case
+    if(index == s.length()) {
+        return open_needed + added;
+    }
+    
+    // recursive step
+    if(s[index] == '('){
+        return solve(s, index + 1, open_needed + 1, added);
+    }
+    else{
+        if (open_needed > 0){
+            return solve(s, index + 1, open_needed - 1, added);
+        } 
+
+        else{
+            return solve(s, index + 1, open_needed, added + 1);
+        }
+    }
+}
+int mininumBracketAdd(string s) {
+    return solve(s, 0, 0, 0);
 }
